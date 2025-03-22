a52c0115db9aa7f202d47d85719286c3a251ae67
BobLiu <bobliu@bobliu.tech>
2025-03-22 16:49:26 +0800

    feat: update compile.sh: add compile success hint

--- docker/scripts/compile.sh
+++ docker/scripts/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -g -O0 /sandbox/code.c -o /sandbox/program > /sandbox/compile.log 2>&1
+gcc -g -O0 /sandbox/code.c -o /sandbox/program > /sandbox/compile.log 2>&1 && echo "Compiled successfully" >> /sandbox/compile.log
